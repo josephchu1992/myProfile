b9e046a042b5555c0d635b6f57da0eb690a39931
josephchu <joseph.chu@hughes.com>
2017-08-14 16:28:42 -0600

    add jquery and ui-router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { UIRouterModule } from '@uirouter/angular';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
@@ -10,7 +11,8 @@ import { AboutComponent } from './about/about.component';
     AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // UIRouterModule.forRoot({ states: [ helloState, aboutState ], useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
